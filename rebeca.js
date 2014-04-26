2f530e06c0d763cc646301cb6d67b5fdb8b4c516
sservinl <rebecaservinlozano@gmail.com>
2014-04-26 13:20:13 -0500

    rebe

--- rebeca.js
+++ rebeca.js
@@ -1,1 +1,15 @@
-esta es una prueba
+var myVar = "rebeca!";
+console.log('my')
+
+var myNum = 20;
+var myArray =["uno", "dos", "tres", "3", 23, [1,2,3]]
+console.log(myNum+10);
+
+console.log(myArray[4][2])
+
+$(document).ready(function(){
+ $('h1').click(function(){
+ 	$('p').css('background-color', 'yellow');
+ });
+
+});
